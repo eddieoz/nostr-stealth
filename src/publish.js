4b7ef40a10c02b27064519dca4164e1163a84eb7
eddieoz <osoriojr@gmail.com>
2023-08-28 21:49:12 +0300

    Commit for github

---
+++ src/publish.js
@@ -0,0 +1,103 @@
+// Import necessary modules from 'nostr-tools' library
+import {
+    generatePrivateKey,
+    getPublicKey,
+    nip04,
+    relayInit,
+    getEventHash,
+    getSignature,
+    nip19
+} from 'nostr-tools'
+import 'websocket-polyfill'
+import 'dotenv/config'
+import * as crypto from "crypto";
+
+// Assign 'crypto' module to global object 'globalThis.crypto'
+globalThis.crypto = crypto;
+
+// Initialize a relay object by calling the 'relayInit' function and passing the WebSocket URL as a parameter
+const relay = relayInit('wss://wallet.sats4.life/nostrrelay/mix')
+
+// Register an event listener for the 'connect' event of the relay object
+relay.on('connect', () => {
+    console.log(`connected to ${relay.url}`)
+})
+
+// Register an event listener for the 'error' event of the relay object
+relay.on('error', () => {
+    console.log(`failed to connect to ${relay.url}`)
+})
+
+// Connect to the relay server by calling the 'connect' method of the relay object
+await relay.connect()
+
+// Managing the keys
+
+// sender 
+let senderPrivkey = process.env.SPRIV
+let senderPubkey = getPublicKey(senderPrivkey)
+
+// receiver 
+let receiverPubkey = process.env.RPUB
+
+// channel pubkey 
+let channelPubkey = process.env.CHANPUB
+
+// ephemeral sender key
+let ephemeralPrivkey = generatePrivateKey()
+let ephemeralPubkey = getPublicKey(ephemeralPrivkey)
+
+// on the sender side
+// Check if a message is provided as a command line argument
+if (process.argv.length <= 2) {
+    console.log("Please provide a message as a command line argument");
+    process.exit(1);
+}
+  
+  // Get the message from the command line argument
+  const message = process.argv[2];
+  
+  // Print the received message
+  console.log("Received message:", message);
+
+// Encrypt the message using the 'nip04' module
+let ciphertext1 = await nip04.encrypt(senderPrivkey, receiverPubkey, message)
+
+// Create an event object kind 4
+let stealth_event = {
+    kind: 4,
+    pubkey: senderPubkey,
+    tags: [['p', receiverPubkey]],
+    content: ciphertext1,
+    created_at: Math.floor(Date.now() / 1000)
+}
+
+// Encrypt the stealth_event using the 'nip04' module
+let ciphertext2 = await nip04.encrypt(senderPrivkey, receiverPubkey, JSON.stringify(stealth_event))
+
+// Create an event object kind 31337
+let event = {
+    kind: 31337,
+    pubkey: ephemeralPubkey,
+    tags: [['p', channelPubkey]],
+    content: ciphertext2,
+    created_at: Math.floor(Date.now() / 1000)
+}
+
+// Sign the event using the 'getSignature' function
+event.id = getEventHash(event)
+event.sig = getSignature(event, ephemeralPrivkey)
+
+// Publish the event using the 'publish' method of the relay object
+let pub = relay.publish(event)
+pub.on('ok', () => {
+    console.log(`${relay.url} has accepted our event`)
+})
+pub.on('failed', reason => {
+    console.log(`failed to publish to ${relay.url}: ${reason}`)
+})
+
+// Disconnect from the relay server by calling the 'close' method of the relay object
+relay.close()
+
+
---
+++ src/receive.js
@@ -0,0 +1,108 @@
+import {
+    generatePrivateKey,
+    getPublicKey,
+    nip04,
+    relayInit,
+    getEventHash,
+    getSignature
+} from 'nostr-tools'
+import 'websocket-polyfill'
+import 'dotenv/config'
+import { secp256k1 } from "@noble/curves/secp256k1";
+import { base64 } from "@scure/base"
+import * as crypto from "crypto";
+
+globalThis.crypto = crypto;
+
+// Test decryption of a message
+async function test_decrypt(privkey, pubkey, data) {
+    var utf8Decoder = new TextDecoder("utf-8");
+    let [ctb64, ivb64] = data.split("?iv=");
+    let key = secp256k1.getSharedSecret(privkey, "02" + pubkey);
+    let normalizedKey = getNormalizedX(key);
+    let cryptoKey = await crypto.subtle.importKey(
+        "raw",
+        normalizedKey,
+        { name: "AES-CBC" },
+        false,
+        ["decrypt"]
+    );
+    let ciphertext = base64.decode(ctb64);
+    let iv = base64.decode(ivb64);
+    try {
+        let plaintext = await crypto.subtle.decrypt(
+            { name: "AES-CBC", iv },
+            cryptoKey,
+            ciphertext
+        );
+        let text = utf8Decoder.decode(plaintext);
+        return text;    
+    } catch {
+        return false;
+    }
+    
+}
+
+// Get the normalized X coordinate of a public key
+function getNormalizedX(key) {
+    return key.slice(1, 33);
+}
+
+// Initialize a relay object by calling the 'relayInit' function and passing the WebSocket URL as a parameter
+const relay = relayInit('wss://wallet.sats4.life/nostrrelay/mix')
+relay.on('connect', () => {
+    console.log(`connected to ${relay.url}`)
+})
+relay.on('error', () => {
+    console.log(`failed to connect to ${relay.url}`)
+})
+
+await relay.connect()
+
+// Managing the keys
+
+// receiver
+let receiverPrivkey = process.env.RPRIV
+let receiverPubkey = getPublicKey(receiverPrivkey)
+
+// channel pubkey
+let channelPubkey = process.env.CHANPUB
+
+// connect to relay and subscribe to events
+let sub = await relay.sub([{
+    kinds: [31337],
+    "#p": [channelPubkey]
+}])
+
+// receive events
+let events = []
+sub.on('event', event => {
+    events.push(event)
+})
+sub.on('eose', () => {
+    decryptMessage(events[0])
+    sub.unsub()
+})
+
+// decrypt message
+async function decryptMessage(event){
+    // Friends Pubs
+    let registeredSenderPubs = [
+        '2a86e9fc098967802cd73fe4390df7013af52358ca1e4e74219631a3c436fdeb',
+    ]
+
+    for (let i in registeredSenderPubs) {
+        
+        // test decryption of the event
+        let stealth_event = await test_decrypt(receiverPrivkey, registeredSenderPubs[i], event.content)
+        // if decryption is successful, decrypt the message
+        if (stealth_event != false) {
+            let message = await nip04.decrypt(receiverPrivkey, JSON.parse(stealth_event).pubkey, JSON.parse(stealth_event).content)
+            // console.log(event)
+            console.log('\nPubkey:', JSON.parse(stealth_event).pubkey, '\n', message, '\n', stealth_event)
+        }
+    }
+    // close the relay connection
+    relay.close()
+}
+
